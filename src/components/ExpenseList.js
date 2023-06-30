c5b9e7c6c2d5e268c4a503f370e5f19d5fb48567
MikeBraunstein <mikebraunstein@duck.com>
2023-06-30 07:56:34 -0400

    Updated all contents in the components folder

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,25 @@
+import React, { useContext } from 'react';
+import ExpenseItem from './AllocationForm';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <table className='table'>
+            <thread className="thread-light">
+          <tr>
+              <th scope="col">Department</th>
+              <th scope="col">Allocated Budget</th>
+              <th scope="col">Increased by 10</th>
+              <th scope="col">Delete</th>
+          </tr>
+        </thread>
+          <tbody>
+          {expenses.map((expenses) => (
+              <ExpenseItem id={expenses.id} key={expenses.id} name={expenses.name} cost={expenses.cost} />
+          ))}
+          </tbody>
+        </table>
+    );
+};
