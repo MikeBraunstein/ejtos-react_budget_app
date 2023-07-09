bb55de7bb21565ceb1c1927e8caabae12f593fbe
MikeBraunstein <mikebraunstein@duck.com>
2023-07-09 01:21:38 -0400

    Updated so that selected value will call the dispatch action to update the state.

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -13,10 +13,10 @@ const Currency = () => {
     return (
         <div className='alert alert-secondary'>
             <select name="CurrencyDropdown" value={currency}>
-                <option name="Dollar" id='Dollar' value="$">$ Dollar</option>
-                <option name="Pound" id='Pound' value="£">£ Pound</option>
-                <option name="Euro" id='Euro' value="€">€ Euro</option> 
-                <option name="Ruppee" id='Ruppee' value="₹">₹ Ruppee</option>
+                <option name="Dollar" id='Dollar' value="$" selected={changeCurrency(option.value)}>$ Dollar</option>
+                <option name="Pound" id='Pound' value="£" selected={changeCurrency(option.value)}>£ Pound</option>
+                <option name="Euro" id='Euro' value="€" selected={changeCurrency(option.value)}>€ Euro</option> 
+                <option name="Ruppee" id='Ruppee' value="₹" selected={changeCurrency(option.value)}>₹ Ruppee</option>
             </select>
         </div>
     )
