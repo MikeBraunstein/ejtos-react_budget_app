6e1ef92add95aceba9e21bb7b538f13211d7020c
MikeBraunstein <mikebraunstein@duck.com>
2023-07-09 01:17:10 -0400

    Updated Currency.js

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -2,14 +2,21 @@ import React, {useContext} from 'react';
 import { AppContext } from './context/AppContext.js'
 
 const Currency = () => {
+    const {currency, dispatch} = useContext(AppContext)
 
+    const changeCurrency = (select) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: select,
+        })
+    }
     return (
         <div className='alert alert-secondary'>
-            <select name="CurrencyDropdown">
-                <option name="Dollar" id='Dollar' value="$ Dollar">$ Dollar</option>
-                <option name="Pound" id='Pound' value="£ Pound">£ Pound</option>
-                <option name="Euro" id='Euro' value="€ Euro">€ Euro</option> 
-                <option name="Ruppee" id='Ruppee' value="₹ Ruppee">₹ Ruppee</option>
+            <select name="CurrencyDropdown" value={currency}>
+                <option name="Dollar" id='Dollar' value="$">$ Dollar</option>
+                <option name="Pound" id='Pound' value="£">£ Pound</option>
+                <option name="Euro" id='Euro' value="€">€ Euro</option> 
+                <option name="Ruppee" id='Ruppee' value="₹">₹ Ruppee</option>
             </select>
         </div>
     )
