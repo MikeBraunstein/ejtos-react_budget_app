8f5a6874867a2794167c1ec451e9c22d5d1b5648
MikeBraunstein <mikebraunstein@duck.com>
2023-07-09 01:41:03 -0400

    Updated <select> so that the onChange event happened there instead of in each option.

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import { AppContext } from './context/AppContext.js'
+import { AppContext } from '../context/AppContext'
 
 const Currency = () => {
     const {currency, dispatch} = useContext(AppContext)
@@ -12,13 +12,15 @@ const Currency = () => {
     }
     return (
         <div className='alert alert-secondary'>
-            <select name="CurrencyDropdown" value={currency}>
-                <option name="Dollar" id='Dollar' value="$" selected={changeCurrency(option.value)}>$ Dollar</option>
-                <option name="Pound" id='Pound' value="£" selected={changeCurrency(option.value)}>£ Pound</option>
-                <option name="Euro" id='Euro' value="€" selected={changeCurrency(option.value)}>€ Euro</option> 
-                <option name="Ruppee" id='Ruppee' value="₹" selected={changeCurrency(option.value)}>₹ Ruppee</option>
+            <select name="Currency" value={currency} onChange={(event) => changeCurrency(event.target.value)}>
+                <option name="Dollar" id='Dollar' value="$">$ Dollar</option>
+                <option name="Pound" id='Pound' value="£">£ Pound</option>
+                <option name="Euro" id='Euro' value="€">€ Euro</option> 
+                <option name="Ruppee" id='Ruppee' value="₹">₹ Ruppee</option>
             </select>
         </div>
     )
 
-}
+};
+
+export default Currency;
